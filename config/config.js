bc1a7f08a2fc062bf64843b0e9f7c87c5c97d2a9
dashcraft <ashcraftbookkeeping@gmail.com>
2020-06-18 18:51:55 -0500

    Updated to include databaseUrl parsing for heroku postgres

--- config/config.js
+++ config/config.js
@@ -1,4 +1,19 @@
 require('dotenv').config()
+let production = {
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": "np_services",
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "dialect": "postgres",
+  "operatorsAliases": false
+}
+
+if(process.env.DATABASE_URL) {
+  production = {
+    url: process.env.DATABASE_URL,
+  }
+}
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
@@ -18,13 +33,5 @@ module.exports = {
     "dialect": "postgres",
     "operatorsAliases": false
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "np_services",
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres",
-    "operatorsAliases": false
-  }
+  "production": production
 }
