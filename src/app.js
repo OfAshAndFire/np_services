4a3e1b241bf22baee0464d6ef05913f4bb396808
Daniel Ashcraft <daniel.ashcraft@ofashandfire.com>
2020-06-18 17:04:54 -0500

    Added swagger jsdoc to generate and scaffold swagger via jsdoc like syntax (#8)
    Co-authored-by: dashcraft <ashcraftbookkeeping@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+/**
+ * @swagger
+ *
+ * /:
+ *   get:
+ *     description: Base Route
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: login
+ *         schema:
+ *            type: string
+ */
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
