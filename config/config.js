b24df0ac47cd207790ad0041e46ad16e4df541dd
dashcraft <ashcraftbookkeeping@gmail.com>
2020-06-18 19:29:22 -0500

    Maybe add strings around url to force it to use url over other configs'

--- config/config.js
+++ config/config.js
@@ -1,19 +1,4 @@
 require('dotenv').config()
-let production = {
-  "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD,
-  "database": "np_services",
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "dialect": "postgres",
-  "operatorsAliases": false
-}
-
-if(process.env.DATABASE_URL) {
-  production = {
-    url: process.env.DATABASE_URL,
-  }
-}
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
