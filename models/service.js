e12735970fef10ea1fabff714e78a07a4f7410bd
Christopher Tse <chris@chris-tse.com>
2020-06-20 18:36:00 -0500

    Add service model with association to category

---
+++ models/service.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const { Sequelize } = require('sequelize/types')
+
+const { v4: uuidV4 } = require('uuid')
+
+module.exports = (sequelize, DataTypes) => {
+    const Service = sequelize.define(
+        'service',
+        {
+            name: {
+                allowNull: false,
+                type: Sequelize.UUID,
+            },
+            description: Sequelize.STRING,
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        },
+        {
+            hooks: {
+                beforeValidate: (service, options) => {
+                    service.id = uuidV4()
+                },
+            },
+        },
+    )
+
+    Service.associate = function (models) {
+        Service.belongsToMany(models.Category, {
+            through: 'ServiceCategories',
+            as: 'categories',
+            foreignKey: 'serviceId',
+            otherKey: 'categoryId',
+        })
+    }
+
+    return Service
+}
