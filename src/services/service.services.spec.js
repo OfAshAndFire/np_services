1ecc325afdbdb4795891a14b547588bfcc677edc
Christopher Tse <chris@chris-tse.com>
2020-06-20 23:27:43 -0500

    Remove unneeded fakeServices list

--- src/services/service.services.spec.js
+++ src/services/service.services.spec.js
@@ -1,42 +1,45 @@
 import '../../test/testSetup'
 import models from '../../models'
-import { FindAllServices, FindServicesById } from './service.services'
-
-const fakeServices = [
-    {
-        id: 1,
-        name: 'service1',
-        description: 'service1',
-    },
-    {
-        id: 2,
-        name: 'service2',
-        description: 'service2',
-    },
-]
+import { FindAllServices, FindServicesById, CreateService, UpdateService } from './service.services'
 
 describe('Testing Service Services Unit Tests', () => {
     it('GET /services should call db.findAll', async () => {
         sandbox.stub(models.Service, 'findAll')
         await FindAllServices()
+
         expect(models.Service.findAll.calledOnce).toBeTruthy()
     })
 
     it('GET /services/id should call db.findOne', async () => {
-        await FindServicesById(mockDb, { id: 1 })
-        expect(mockDb.Service.findOne).toHaveBeenCalledWith({ id: 1 })
+        sandbox.stub(models.Service, 'findOne')
+        await FindServicesById({ id: 1 })
+
+        expect(models.Service.findOne.calledOnce).toBeTruthy()
     })
 
-    it('PUT /services/id should call db.findOne', async () => {
-        await UpdateCategory(mockDb, { id: 1 }, { name: 'test' })
-        expect(mockDb.Category.update).toHaveBeenCalledWith(
-            { name: 'test' },
-            {
+    it('POST /services should call db.create', async () => {
+        sandbox.stub(models.Service, 'create')
+        await CreateService({ name: 'new service', description: 'new description' })
+
+        expect(models.Service.create.calledOnce).toBeTruthy()
+    })
+
+    it('PUT /services/id should call db.update', async () => {
+        const updatedService = {
+            name: 'updated service name',
+            description: 'updated service description',
+        }
+
+        sandbox.stub(models.Service, 'update').resolves([1, [{ id: 1, ...updatedService }]])
+        await UpdateService({ id: 1 }, updatedService)
+
+        expect(
+            models.Service.update.calledWith(updatedService, {
                 returning: true,
                 where: {
                     id: 1,
                 },
-            },
-        )
+            }),
+        ).toBeTruthy()
     })
 })
