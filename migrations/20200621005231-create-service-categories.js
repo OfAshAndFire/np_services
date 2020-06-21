c222baf8acf9a037c1516886f63cde7d03b4cac8
Christopher Tse <chris@chris-tse.com>
2020-06-20 23:31:21 -0500

    Remove extra id field

--- migrations/20200621005231-create-service-categories.js
+++ migrations/20200621005231-create-service-categories.js
@@ -1,33 +1,29 @@
-'use strict';
+'use strict'
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('ServiceCategories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.UUID
-      },
-      id: {
-        type: Sequelize.UUID
-      },
-      categoryId: {
-        type: Sequelize.UUID
-      },
-      serviceId: {
-        type: Sequelize.UUID
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('ServiceCategories');
-  }
-};
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('ServiceCategories', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+            },
+            categoryId: {
+                type: Sequelize.UUID,
+            },
+            serviceId: {
+                type: Sequelize.UUID,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('ServiceCategories')
+    },
+}
