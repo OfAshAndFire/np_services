9735404ed2d6d3f0b8d87cde69c63beaa1801681
Christopher Tse <chris@chris-tse.com>
2020-06-21 21:31:58 -0500

    Remove operatorAliases to get rid of deprecation warning

--- config/config.js
+++ config/config.js
@@ -1,34 +1,32 @@
 require('dotenv').config()
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "np_services_dev",
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "np_services_test",
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres",
-    "operatorsAliases": false
-  },
-  "production": {
-    "url": process.env.DATABASE_URL,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'np_services_dev',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
     },
-    "database": "np_services",
-    "operatorsAliases": false,
-    "use_env_variable": "DATABASE_URL"
-  }
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'np_services_test',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+    },
+    production: {
+        url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        database: 'np_services',
+        use_env_variable: 'DATABASE_URL',
+    },
 }
